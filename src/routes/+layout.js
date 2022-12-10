eba9d524bd7ab04a333cfec3626bc74649193f25
Joe Astuccio <joe@astucc.io>
2022-12-10 12:27:52 -0500

    Add +layout.js

---
+++ src/routes/+layout.js
@@ -0,0 +1,25 @@
+import Feature from "../components/Feature.svelte";
+import Grid from "../components/Grid.svelte";
+import Page from "../components/Page.svelte";
+import Teaser from "../components/Teaser.svelte";
+import { apiPlugin, storyblokInit, useStoryblokApi } from "@storyblok/svelte";
+
+/** @type {import('./$types').LayoutLoad} */
+export async function load() {
+  storyblokInit({
+    accessToken: "W1vLyxT5rQ15jBpANjnv0gtt",
+    use: [apiPlugin],
+    apiOptions: { region: 'us' }, //? is this required here
+    components: {
+      feature: Feature,
+      grid: Grid,
+      page: Page,
+      teaser: Teaser,
+    },
+  });
+  let storyblokApi = await useStoryblokApi();
+
+  return {
+    storyblokApi: storyblokApi,
+  };
+}
