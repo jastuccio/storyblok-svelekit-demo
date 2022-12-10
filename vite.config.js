c1bc9070bec7ae4e77189c1694d87e74311270f0
Joe Astuccio <joe@astucc.io>
2022-12-10 10:29:11 -0500

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,29 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import mkcert from 'vite-plugin-mkcert';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+  plugins: [sveltekit(), mkcert()],
+  server: {
+    https: true,
+    apiOptions: { region: 'us' },
+    /************** 
+     * `proxy: {}` is suggested as a bug fix
+     *  https://stackoverflow.com/questions/73205096/run-sveltekit-dev-with-https
+     * */
+    // proxy: {},
+  },
 };
 
 export default config;
+
+/*************************************************** */
+
+// import { sveltekit } from '@sveltejs/kit/vite';
+
+// /** @type {import('vite').UserConfig} */
+// const config = {
+//   plugins: [sveltekit()]
+// };
+
+// export default config;
